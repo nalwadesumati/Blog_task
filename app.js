dab88af86eb5cd53f3c47f18e468ff204ac0ae24
nalwadesumati <165995604+nalwadesumati@users.noreply.github.com>
2025-11-10 15:03:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+const cl = console.log;
+
+
+const blogForm = document.getElementById("blogForm");
+const blogTitleCtrl = document.getElementById("blogTitle");
+const blogContentctrl = document.getElementById("blogContent");
+const blogContainer = document.getElementById("blogContainer");
+
+
+let blogsArr = [];
+
+function snackBar(msg, icon) {
+    Swal.fire({
+        title: "Something went wrong while creating new Blog !!!",
+        icon: "error",
+        timer: 2500
+    })
+}
+
+
+
+function onBlogSubmit(eve) {
+    eve.preventDefault();
+
+    let blogObj = {
+        blogTitle: blogTitleCtrl.value,
+        blogContent: blogContentctrl.value
+    };
+
+    onPostblog(blogObj);
+}
+
+
+const onPostblog = (obj) => {
+    setTimeout(() => {
+        let error1 = Math.random() > .5 ? false : true;
+        if (!error1) {
+            blogsArr.push(obj);
+            onFetchBlog(blogsArr)
+
+        } else {
+            snackBar('Something went wrong while creating new Blog !!!', 'error')
+        }
+    }, 2000);
+}
+
+
+
+const onFetchBlog = (arr) => {
+    setTimeout(() => {
+        let error = Math.random() > .5 ? false : true;
+
+        if (!error) {
+            let data = arr;
+
+            temp(data);
+        } else {
+            snackBar('Something went wrong while creating new Blog !!!', 'error')
+
+        }
+    }, 1000)
+}
+
+
+const temp = (data) => {
+    let result = "";
+
+    data.forEach(blog => {
+        result +=
+            ` <div class="col-md-3 col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                      <h5>${blog.blogTitle}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p>${blog.blogContent}</p>
+                    </div>
+                </div>
+            </div>`
+
+    })
+    blogContainer.innerHTML = result;
+
+}
+
+
+
+blogForm.reset();
+
+
+
+blogForm.addEventListener("submit", onBlogSubmit)
