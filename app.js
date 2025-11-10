05230d32615540bf14555809cad45a54026dc22b
nalwadesumati <165995604+nalwadesumati@users.noreply.github.com>
2025-11-10 16:15:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const cl = console.log;
+
 const blogForm = document.getElementById("blogForm");
 const blogTitleCtrl = document.getElementById("blogTitle");
 const blogContentCtrl = document.getElementById("blogContent");
 const blogContainer = document.getElementById("blogContainer");
 
 let blogsArr = [];
+
 
 function snackBar(icon, msg) {
     Swal.fire({
@@ -16,52 +19,67 @@ function snackBar(icon, msg) {
     });
 }
 
+
 function onBlogSubmit(e) {
     e.preventDefault();
+
+
+
 
     const blogObj = {
         blogTitle: blogTitleCtrl.value,
         blogContent: blogContentCtrl.value
     };
 
-    if (!blogObj.blogTitle || !blogObj.blogContent) {
-        snackBar("warning", "Please enter both title and content!");
-        return;
-    }
-
     onPostBlog(blogObj);
 }
 
+
 const onPostBlog = (obj) => {
+    setTimeout(() => {
+        let error1 = Math.random() > 0.4 ? false : true;
+        if (!error1) {
+            blogsArr.push(obj);
+            onFetchBlog(blogsArr);
+            snackBar("success", "Blog added successfully!");
 
-    blogsArr.push(obj);
-    onFetchBlog(blogsArr);
-    snackBar("success", "Blog added successfully!");
 
-
-    blogTitleCtrl.value = "";
-    blogContentCtrl.value = "";
+            blogTitleCtrl.value = "";
+            blogContentCtrl.value = "";
+        } else {
+            snackBar("error", "Something went wrong while posting the blog");
+        }
+    }, 2000);
 }
+
 
 const onFetchBlog = (arr) => {
-
-    renderBlogs(arr);
+    setTimeout(() => {
+        let error = Math.random() > 0.5 ? false : true;
+        if (!error) {
+            renderBlogs(arr);
+        } else {
+            snackBar("error", "Fetch failed, showing old data");
+            renderBlogs(arr);
+        }
+    }, 1000);
 }
 
-const renderBlogs = (blogs) => {
+
+const renderBlogs = (data) => {
     let result = "";
-    blogs.forEach(blog => {
+    data.forEach(blog => {
         result += `
-      <div class="col-md-3 col-sm-6 mb-4">
-        <div class="card">
-          <div class="card-header">
-            <h5>${blog.blogTitle}</h5>
-          </div>
-          <div class="card-body">
-            <p>${blog.blogContent}</p>
-          </div>
-        </div>
-      </div>`;
+        <div class="col-md-3 col-sm-6 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5>${blog.blogTitle}</h5>
+                </div>
+                <div class="card-body">
+                    <p>${blog.blogContent}</p>
+                </div>
+            </div>
+        </div>`;
     });
 
     blogContainer.innerHTML = result;
@@ -69,4 +87,3 @@ const renderBlogs = (blogs) => {
 
 
 blogForm.addEventListener("submit", onBlogSubmit);
-
